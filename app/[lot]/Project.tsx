3dc0f90728e9c5d34895ea021160c12336628fcb
Aite Aigbe <aite@nabla.ooo>
2023-01-05 21:50:53 +0100

    video styling

--- app/[lot]/Project.tsx
+++ app/[lot]/Project.tsx
@@ -6,7 +6,7 @@ export function Project({ ...props }) {
   const { category, client, name, type, description, content, by, lot } =
     props.project;
   return (
-    <div className="flex h-full w-full flex-col items-center justify-start lg:px-12 ">
+    <div className="grid h-full w-full grid-cols-[100%] grid-rows-[fit-content_max-content] flex-col items-center justify-start overflow-y-scroll md:flex lg:px-12">
       {/* Metadata */}
       <div className="2flex w-full flex-col items-start justify-center gap-y-2 md:mx-7 md:grid md:h-60 md:grid-cols-[2fr_1fr] md:grid-rows-[1fr]">
         <div className="md:w-full">
@@ -20,30 +20,44 @@ export function Project({ ...props }) {
             }`}</p>
           ) : null}
         </div>
-        <div className="relative h-auto overflow-y-auto md:max-w-prose">
+        <div className="relative overflow-y-auto md:max-w-prose">
           {description ? (
-            <p className="description">{`"${description}" - ${by}`}</p>
+            <p className=" h-2/4">{`"${description}" - ${by}`}</p>
           ) : null}
         </div>
       </div>
       {/* carosel */}
-      <div className="carousel-center carousel rounded-box h-full w-full space-x-4 bg-white bg-opacity-70 p-4 dark:bg-black dark:bg-opacity-70 md:max-w-[80%]">
-        {content &&
-          content?.map((value: any, index: number) => (
-            <Content value={value} key={index} />
-          ))}
+      <div className="flex h-full w-full flex-col items-center justify-center gap-2 py-2">
+        <div className="carousel-center carousel rounded-box h-[40vh] w-full space-x-4 bg-white bg-opacity-70 p-4 pb-1 dark:bg-black dark:bg-opacity-70 md:max-w-[80%]">
+          {content &&
+            content?.map((value: any, index: number) => (
+              <Content value={value} key={index} index={index} />
+            ))}
+        </div>
+        {content && content.length > 1 ? (
+          <div className="flex w-full justify-center gap-2 py-2">
+            {content.map((value: any, index: number) => (
+              <a href={`#item${index}`} className="link h-min w-min">
+                {index}
+              </a>
+            ))}
+          </div>
+        ) : null}
       </div>
     </div>
   );
 }
 
 function Content({ ...props }) {
-  const { value } = props;
-  const { url, name, type, caption } = value;
+  const { value, index } = props;
+  const { url, name, type, caption, orientation } = value;
   return (
     <>
       {type === "image" ? (
-        <div className="carousel-item relative h-auto w-96 overflow-hidden">
+        <div
+          id={`item${index}`}
+          className="carousel-item relative h-auto w-96 overflow-hidden"
+        >
           <Image
             src={url}
             fill
@@ -59,23 +73,50 @@ function Content({ ...props }) {
         </div>
       ) : //   TODO: add video styling
       type === "video" ? (
-        <div className="carousel-item relative h-auto w-40 overflow-hidden">
-          <video
-            className="rounded-md shadow-md"
-            autoPlay
-            loop
-            muted
-            playsInline
+        orientation === "portrait" ? (
+          <div
+            id={`item${index}`}
+            className="carousel-item relative h-auto w-40 overflow-hidden"
           >
-            <source src={url} type="video/mp4" /> Your browser does not support
-            the video tag. I suggest you upgrade your browser.
-          </video>
-          {caption ? (
-            <p className="absolute bottom-0 w-full bg-white bg-opacity-70 p-1 text-xs backdrop-blur-md dark:bg-black dark:bg-opacity-70">
-              {caption}
-            </p>
-          ) : null}
-        </div>
+            <video
+              className="rounded-md shadow-md"
+              autoPlay
+              loop
+              muted
+              playsInline
+            >
+              <source src={url} type="video/mp4" /> Your browser does not
+              support the video tag. I suggest you upgrade your browser.
+            </video>
+            {caption ? (
+              <p className="absolute bottom-0 w-full bg-white bg-opacity-70 p-1 text-xs backdrop-blur-md dark:bg-black dark:bg-opacity-70">
+                {caption}
+              </p>
+            ) : null}
+          </div>
+        ) : (
+          // Landscape
+          <div
+            id={`item${index}`}
+            className="carousel-item relative h-full w-fit overflow-hidden"
+          >
+            <video
+              controls
+              className="rounded-md shadow-md"
+              autoPlay={false}
+              muted={false}
+              playsInline
+            >
+              <source src={url} type="video/mp4" /> Your browser does not
+              support the video tag. I suggest you upgrade your browser.
+            </video>
+            {caption ? (
+              <p className="absolute top-0 w-full bg-white bg-opacity-70 p-1 text-xs backdrop-blur-md dark:bg-black dark:bg-opacity-70">
+                {caption}
+              </p>
+            ) : null}
+          </div>
+        )
       ) : null}
     </>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,9 @@ export default async function RootLayout({
       <body className="h-full  bg-white text-blue-900 selection:bg-blue-900 selection:bg-opacity-60 selection:text-white dark:bg-black dark:text-blue-200 dark:selection:bg-blue-200 selection:dark:bg-opacity-60 dark:selection:text-black ">
         <Init data={data}>
           <div className="flex h-full w-screen flex-col justify-start overflow-x-hidden  p-2 md:px-[10px] md:pb-2">
-            <div className="h-full py-20 md:pb-6">{children}</div>
+            <div className="h-full pt-14 pb-24 md:py-20 md:pb-6">
+              {children}
+            </div>
           </div>
         </Init>
         <Backdrop />
