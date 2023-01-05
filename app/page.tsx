a8e914ab622aa8d2e04bfd352ddc5a3bb4d788df
Aite Aigbe <aite@nabla.ooo>
2023-01-05 21:54:58 +0100

    Serialize data

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ import ProjectLink from "./ProjectLink";
 import { getData } from "@api/api";
 
 export default async function HomePage() {
-  // scroll to the top when the component mounts
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, []);
   const data = await getData();
 
-  // TODO: turn the date or each project into strings
-  const dataString = JSON.stringify(data);
-  console.log(dataString);
+  // Serialize the date object to a string to prevent the error
+  data.map((doc: any) => {
+    const { date } = doc;
+    date.seconds = date.seconds.toString();
+    date.nanoseconds = date.nanoseconds.toString();
+    console.log(date);
+  });
 
   return (
     <div className="mx-1 w-full pb-24 md:pb-6 lg:px-[10rem] xl:px-[20rem]">
