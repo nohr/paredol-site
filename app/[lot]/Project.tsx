421232d34cf417d8055db8bcda9a3ade598f7f54
Aite Aigbe <aite@nabla.ooo>
2023-01-07 14:18:43 +0100

    modal

--- app/[lot]/Project.tsx
+++ app/[lot]/Project.tsx
@@ -1,11 +1,37 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import { IoCloseSharp } from "react-icons/io5";
 
 export function Project({ ...props }) {
   const { category, client, name, type, description, content, by, lot, url } =
     props.project;
+
+  // handle modal state and content
+  const [modal, setModal] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+
   return (
     <div className="grid h-full w-full grid-cols-[100%] grid-rows-[fit-content_max-content] flex-col  items-center justify-start overflow-y-scroll md:flex md:w-fit md:grid-rows-[1fr_3fr] lg:px-12">
+      {/* Modal */}
+      {modal && modalContent ? (
+        <div className="absolute top-0 left-0 z-50 h-full w-full">
+          <div className="flex h-full w-full flex-col items-center justify-center">
+            {modalContent}
+            <button
+              className="absolute top-3 right-3 z-[70] rounded-full border-white bg-black p-4 dark:border-black dark:bg-white"
+              onClick={() => {
+                setModal(false);
+                setModalContent(null);
+              }}
+            >
+              <IoCloseSharp className="h-8 w-auto fill-white dark:fill-black" />
+            </button>
+          </div>
+        </div>
+      ) : null}
       {/* Metadata */}
       <div className="2flex w-full flex-col items-start justify-center gap-y-2 md:mx-0 md:grid md:h-60 md:grid-cols-[2fr_1fr] md:grid-rows-[1fr]">
         <div className="flex flex-col gap-y-1 md:w-full">
@@ -38,10 +64,16 @@ export function Project({ ...props }) {
       </div>
       {/* carosel */}
       <div className="flex h-full w-full flex-col items-center justify-center gap-2 py-2">
-        <div className="carousel-center rounded-box flex h-[40vh] w-full space-x-4 overflow-x-scroll bg-white bg-opacity-70 p-4 pb-1 dark:bg-black dark:bg-opacity-70 md:h-full md:max-w-[80%]">
+        <div className="carousel-center rounded-box flex h-[40vh] w-full space-x-4 overflow-x-scroll bg-white bg-opacity-70 p-4 pb-1 dark:bg-black dark:bg-opacity-70 md:h-full md:max-w-[100%]">
           {content &&
             content?.map((value: any, index: number) => (
-              <Content value={value} key={index} index={index} />
+              <Content
+                value={value}
+                key={index}
+                setModalContent={setModalContent}
+                setModal={setModal}
+                index={index}
+              />
             ))}
         </div>
         {content && content.length > 1 ? (
@@ -63,22 +95,34 @@ export function Project({ ...props }) {
 }
 
 function Content({ ...props }) {
-  const { value, index } = props;
+  const { value, index, setModalContent, setModal } = props;
   const { url, name, type, caption, orientation } = value;
   return (
     <>
       {type === "image" ? (
         <div
           id={`item${index}`}
-          className="carousel-item relative aspect-auto h-auto w-[inherit] overflow-hidden"
+          className="carousel-item relative aspect-auto h-auto w-20 overflow-hidden"
         >
           <Image
             src={url}
             fill
             alt={name}
-            className="aspect-auto rounded-md shadow-md"
+            className="aspect-auto cursor-pointer rounded-md shadow-md"
             style={{ objectFit: "cover" }}
             sizes="100vw"
+            onClick={() => {
+              setModalContent(
+                <Image alt={name} src={url} fill sizes="100vw" />
+              );
+              setModal(true);
+            }}
+            onTouchEnd={() => {
+              setModalContent(
+                <Image alt={name} src={url} fill sizes="100vw" />
+              );
+              setModal(true);
+            }}
           />
           {caption ? (
             <p className="absolute bottom-0 w-full bg-white bg-opacity-70 p-1 text-xs backdrop-blur-md dark:bg-black dark:bg-opacity-70">
@@ -136,3 +180,22 @@ function Content({ ...props }) {
     </>
   );
 }
+
+//  function Modal({  }) {
+//   const selected = clip.modalImg;
+//   const project = clip.project;
+
+//   const handleClick = (e) => {
+//     if (e.target.classList.contains("backdrop")) cloud.modalImg = null;
+//   };
+
+//   return (
+//     <div className="modalWrapper">
+//       {/* <Controls selected={selected} project={project} /> */}
+//       <div className="backdrop" onClick={handleClick}>
+//         <Content selected={selected} project={project} />
+//         {/* <Caption caption={selected.caption} /> */}
+//       </div>
+//     </div>
+//   );
+// };
